b9f8e57ec9c0ec66a5be2d656e273a6041e94d8c
Li <lijienan2016@outlook.com>
2022-07-09 12:24:01 +0800

    项目-13把token持久化存储到本地

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // 1.存储登录成功后,得到的token
     token: ''
   },
   getters: {
   },
   mutations: {
+    // 2.更新token的mutation函数
     updateToken (state, newToken) {
       state.token = newToken
     }
@@ -17,5 +20,7 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
-  }
+  },
+  // 配置为 vuex 的插件
+  plugins: [createPersistedState()]
 })
