373693efd766a7e32a854c94f042c34505600b00
Li <lijienan2016@outlook.com>
2022-07-11 14:18:45 +0800

    项目25-未登录无法看到正常页面

--- src/router/index.js
+++ src/router/index.js
@@ -27,12 +27,22 @@ const routes = [
 const router = new VueRouter({
   routes
 })
+const whiteList = ['/reg', '/login']
 router.beforeEach((to, from, next) => {
   const token = store.state.token
-  if (token && !store.state.userInfo.username) { // token 这里是全局路由守卫,所以当我们点击退出后也会判断,无token报错所以当token存在时在判断,!store.state.userInfo.username 这句话的意思是 登陆之后 跳转之间页面 不要重复发起请求
-    store.dispatch('getUserInfoActions')
+  if (token) {
+    if (!store.state.userInfo.username) { // token 这里是全局路由守卫,所以当我们点击退出后也会判断,无token报错所以当token存在时在判断,!store.state.userInfo.username 这句话的意思是 登陆之后 跳转之间页面 不要重复发起请求
+      store.dispatch('getUserInfoActions')
+    }
+    next()
+    // next如果强制切换路由地址 会再次走路由守卫
+  } else {
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next('/login')
+    }
   }
-  next()
 })
 
 export default router
