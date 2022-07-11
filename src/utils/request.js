d08d98e73da0691557584eff6b306e983f5e0327
Li <lijienan2016@outlook.com>
2022-07-11 11:03:40 +0800

    项目24-axios请求拦截器,统一携带token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,18 @@
-import theAxios from 'axios'
-const axios = theAxios.create({
+import axios from 'axios'
+import store from '@/store'
+const myAxios = axios.create({
   baseURL: 'http://big-event-vue-api-t.itheima.net',
   timeout: 50000
 })
+myAxios.interceptors.request.use(config => {
+  if (store.state.token) {
+    config.headers.Authorization = store.state.token
+  }
+  return config
+}, error => Promise.reject(error))
 
 export default ({ url, method = 'GET', headers = {}, params = {}, data = {} }) => {
-  return axios(
+  return myAxios(
     {
       url,
       method,
